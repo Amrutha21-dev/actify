7dd32f009d0d70b44cfd7d5a15767e0dda008d90
Amruthavarshini R <ammi.varsha@gmail.com>
2021-02-26 14:36:26 +0530

    Initial spring boot application setup

---
+++ src/main/java/com/example/actify/ActifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.actify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActifyApplication.class, args);
+	}
+
+}
